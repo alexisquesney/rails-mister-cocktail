10847e4f5b078fef3d4251e301ffc33fc77cfbb9
Alexis Quesney <alexis.quesney@ecota.co>
2016-06-02 11:38:33 +0200

    rake ok

--- app/models/dose.rb
+++ app/models/dose.rb
@@ -2,4 +2,7 @@ class Dose < ActiveRecord::Base
   validates :description, presence: true
   belongs_to :cocktail
   belongs_to :ingredient
+  validates :cocktail_id, presence: true
+  validates :ingredient_id, presence: true
+  validates_uniqueness_of :cocktail_id, :scope => [:ingredient_id]
 end
